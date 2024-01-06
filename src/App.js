82ece841ab28eeee97798308eef210b935bee32a
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2024-01-06 17:06:31 -0600

    Add Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Header from './Header/Header.js';
 import Home from './Home/Home.js';
 import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+      </Routes>
     </div>
   );
 }
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,10 +1,16 @@
 import './Header.scss';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
 
   return(
     <header className="App-header">
       <h1>Mormor's Receptbok</h1>
+      <div className="btmbox">
+        <div className="links">
+          <NavLink exact to='/' className="navBtn hovershdw" activeClassName="selected">Home</NavLink>
+        </div>
+      </div>
     </header>
   )
 };
