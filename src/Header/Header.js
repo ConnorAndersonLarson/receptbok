d2a09cfb8a1dc52ed61f0b5f774a814883e13545
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2024-01-06 16:39:03 -0600

    Create Header and Home components

---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.scss';
+
+function Header() {
+
+  return(
+    <header className="App-header">
+      <h1>Mormor's Receptbok</h1>
+    </header>
+  )
+};
+
+export default Header;
---
+++ src/Home/Home.js
@@ -0,0 +1,21 @@
+import './Home.scss';
+
+function Home() {
+
+  return(
+    <main>
+      <h2>Välkommen!</h2>
+      <p>
+        This is a collection of recipes from Inga Sandberg, our grandmother (mormor).
+        
+        Originally created for family to more easily access these easily lost fragments of familial history,
+        we welcome anyone to come enjoy the food passed down in our family and hope they bring the same
+        warmth and joy to you and yours. Håll till godo!
+      </p>
+      <div class="img-box">
+      </div>
+    </main>
+  )
+}
+
+export default Home;
