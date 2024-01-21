26e2ea5b6b3f8ce4b18940c10eb59f7ec9a53ae9
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2024-01-21 16:55:46 -0600

    Add Test Recipe and picture

---
+++ test/test_recipe1.js
@@ -0,0 +1,41 @@
+var recipes = [
+  [
+    name: "Skorpa",
+    shortInfo: "Skorpa (translation: 'Swedish toast', plural: Skorpor) is a form of 'rusk', a dry biscuit or twice-baked bread.",
+    info: "Traditionally left unflavored, or flavored with cardamom, this recipe instead uses sugar and cinnamon to create a sweeter version that is a perfect companion for your morning coffee. Mormor would make this in bulk and have a large jar stocked at all times.",
+    ingredients: [
+      [
+        quantity: "1 Loaf",
+        ingName: "Bakers Bread, white, ideally one week old, unsliced"
+      ],[
+        quantity: "1/2 cup",
+        ingName: "salted butter"
+      ],[
+        quantity: "1/2 cup",
+        ingName: "margarine"
+      ],[
+        quantity: "1.5 cup",
+        ingName: "white sugar"
+      ],[
+        quantity: "2-3 tsp",
+        ingName: "cinnamon"
+      ]
+    ],
+    instructions: [
+      "Preheat oven to 220 degrees, line a baking sheet with foil or parchment paper for easier cleanup",
+      "Melt butter and margarine together",
+      "Combine sugar and cinnamon in a small bowl",
+      "Slice bread into ~1 inch thick slices.",
+      "Quickly dip one slice at a time into the melted butter and margarine, scraping off excess before stacking on a cutting board",
+      "Cut each slice into 4 pieces, horizontally (one piece from each slice will have the entire top crust on it)",
+      "Sprinkle each piece with sugar-cinnamon mixture, setting finished pieces on prepared baking sheet, leaving some space between each piece",
+      "Bake for 4 hours, flipping halfway through"
+    ],
+    keywords: ["Skorpa", "bread", "coffee", "biscuit", "cinnamon", "sugar", "appetizer"],
+    foodType: "appetizer",
+    personalComments: "I remember always wanting skorpa while we waited to have Wednesday night dinners. I never ate them with coffee, as a kid the sweetness was all I needed. I would always search for the pieces with the most crust, as those were usually the sweetest. - Connor, grandson"
+    recipePic: [] ,
+    foodPic: "" ,
+    comments: ["array (not MVP)", "excellent!"]
+  ]
+]
